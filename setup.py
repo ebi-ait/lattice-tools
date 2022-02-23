d4ce7424823fcf90e9855e558e3eec2d43516a21
Jacob Windsor <me@jcbwndsr.com>
2022-02-23 15:19:02 +0100

    Package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='lattice-tools',
+    version='0.1.0',
+    packages=['lattice'],
+    url='https://github.com/Lattice-Data/lattice-tools/',
+    license='MIT',
+    author='Lattice Data Coordination',
+    author_email='',
+    description='External scripts used to interact with the Lattice Database'
+)
