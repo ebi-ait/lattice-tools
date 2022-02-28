f48d0530e97e311d6deac01c0983f4083a638430
Jacob Windsor <me@jcbwndsr.com>
2022-02-28 15:30:21 +0100

    Update requirements

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import os
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 base_dir = os.path.dirname(__file__)
 install_requires = [line.rstrip() for line in open(os.path.join(base_dir, 'requirements.txt'))]
@@ -8,11 +8,14 @@ install_requires = [line.rstrip() for line in open(os.path.join(base_dir, 'requi
 setup(
     name='lattice-tools',
     version='0.1.0',
-    packages=['lattice'],
+    packages=find_packages(),
     url='https://github.com/Lattice-Data/lattice-tools/',
     license='MIT',
     author='Lattice Data Coordination',
     author_email='',
     description='External scripts used to interact with the Lattice Database',
+    entry_points={
+        'console_scripts': ['lattice-flattener=lattice.flattener:main'],
+    },
     install_requires=install_requires
 )
